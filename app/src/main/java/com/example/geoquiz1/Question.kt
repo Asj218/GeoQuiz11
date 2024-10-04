16cf61b1674d3b4969e9b1f053b2ef193ddd09cf
Анастасия <azizovaa031@gmail.com>
2024-10-04 21:57:15 +0700

    Новая кнопка next и изменения в TextView

--- app/src/main/java/com/example/geoquiz1/Question.kt
+++ app/src/main/java/com/example/geoquiz1/Question.kt
@@ -1,4 +1,6 @@
 package com.example.geoquiz1
 
-class Question {
-}
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId:
+                    Int, val answer: Boolean)

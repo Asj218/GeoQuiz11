34a27b859e32ed8f1659e7be5c1a4977734c3bf3
Анастасия <azizovaa031@gmail.com>
2024-10-04 20:13:58 +0700

    Добавление полей

--- app/src/main/java/com/example/geoquiz1/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz1/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.example.geoquiz1
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-
-        }
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
     }
+
 }

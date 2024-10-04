d062796fda6904dfb9d91b7ca2d93bedcf821acd
Анастасия <azizovaa031@gmail.com>
2024-10-04 22:18:55 +0700

    Подключение виджета

--- app/src/main/java/com/example/geoquiz1/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,10 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
@@ -28,6 +33,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
 
         trueButton.setOnClickListener { View ->
             Toast.makeText(
@@ -44,6 +52,9 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT)
                 .show()
         }
+        val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+
 
     }
 
